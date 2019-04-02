9b70718f66ff8d4cdc360bffaaea5df22bf7222f
Imalka Gunawardana <imalkagunawardana1@gmail.com>
2019-04-02 22:44:04 +0530

    up

--- Backend/models/clinicPatients.js
+++ Backend/models/clinicPatients.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose');
+
+var ClinicPatients = mongoose.model('ClinicPatients', {
+    patientId: { type: ObjectId },
+    clinicTime: { type: String },
+    clinicDate: { type: Date }
+})
+
+module.exports = { ClinicPatients };
